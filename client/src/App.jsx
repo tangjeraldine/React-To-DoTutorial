fd47413887dc8c08f6d347e040207f9c89714146
tangjeraldine <typjera.1127@gmail.com>
2022-09-26 11:58:28 +0800

    create browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,37 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Todos from "./routes/Todos";
+import TodosNew from "./routes/TodosNew";
+
+// const TodoLoader = () => [1, 2, 3];
+const TodoLoader = async () => {
+  const res = await fetch("/api/todos/");
+  const data = await res.json();
+  return data;
+}; //! This method allows you to do the fetch in your loader directly in the App.jsx
+
+const router = createBrowserRouter([
+  { path: "/", loader: TodoLoader, element: <TodosNew /> },
+]);
+
+const router2 = createBrowserRouter([
+  { path: "/", loader: TodoLoader, element: <TodosNew /> },
+]);
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Todos />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
+
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<Todos />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
---
+++ client/src/routes/TodosNew.jsx
@@ -0,0 +1,27 @@
+// import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Todos() {
+  // const [todos, setTodos] = useState([]);
+  // useEffect(() => {
+  //   const fetchTodos = async () => {
+  //     const res = await fetch("/api/todos/");
+  //     const data = await res.json();
+  //     setTodos(data);
+  //   };
+  //   fetchTodos();
+  // }, []);
+  const todos = useLoaderData();
+  return (
+    <div>
+      <h3>Todos!</h3>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.title}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todos;
